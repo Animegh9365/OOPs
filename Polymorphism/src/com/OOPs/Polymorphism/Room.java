f90f78706c5ad44976568fcbece3777a88da64a7
Animegh9365 <animeghsarania9365@gmail.com>
2024-09-24 23:15:42 +0530

    Polymorphism example Room.java added

---
+++ Polymorphism/src/com/OOPs/Polymorphism/Room.java
@@ -0,0 +1,15 @@
+package com.OOPs.Polymorphism;
+
+public class Room {
+	public void book() {
+		System.out.println("Room booked efficiently");
+	}
+	
+	public void checkIn() {
+		System.out.println("Guest checked in smoothly");
+	}
+	
+	public void checkOut() {
+		System.out.println("Guest checked out successfully");
+	}
+}
